110afee7ba921ebd3f4061117ce749ce72b1a27a
Jotxee <jotxee@jotxee.dev>
2022-01-09 00:06:19 +0100

    vuex y su store para estado global

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    contador: 100
   },
   mutations: {
+    incrementar(state) {
+      state.contador += 10;
+    }
   },
   actions: {
+    accionIncrementarContador ({commit}) {
+      commit('incrementar')
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <h1>Mi contador: {{ $store.state.contador }}</h1> -->
+
+    <!-- tener condicionales dentro de los tag es incorrecto por lo tanto lo movemos a computed: 
+    <h1
+      :style="[contador >100 ? {'color':'green'} : {'color':'red'}]" 
+    >Mi contador: {{ contador }}</h1>
+    </div>
+     -->
+
+  
+    <h1
+      :style="colotContador" 
+    >{{ titulo }} {{ contador }}</h1>
+    <button @click="accionIncrementarContador">Aumentar contador</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapState, mapMutations, mapActions } from 'vuex'
 export default {
   name: 'Home',
+  data() {
+    return {
+      titulo: 'Mi contador Vuex:'
+    }
+  },
   components: {
-    HelloWorld
+
+  },
+  computed: {
+    ...mapState(['contador']),
+    colotContador() {
+      return [this.contador >100 ? {'color':'green'} : {'color':'red'}]
+    }
+  },
+  methods: {
+    ...mapMutations(['incrementar']), //No se recomienda traer las mutaciones ya que su función es modificar el estado para eso usar las acciones con evento commit
+    ...mapActions(['accionIncrementarContador'])
   }
+  
 }
 </script>
