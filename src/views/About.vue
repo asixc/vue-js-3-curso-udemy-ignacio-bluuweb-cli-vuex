ec2146414cd53be7a79edad29e259ac63693e163
Jotxee <jotxee@jotxee.dev>
2022-01-22 17:09:30 +0100

    add v-model

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Mi contador: {{ $store.state.contador }} </h2>
+    <input type="text" v-model="texto">
+    <h3>{{ texto }}</h3>
   </div>
 </template>
+
+<script>
+
+export default {
+  data() {
+    return  {
+      texto: ''
+    }
+  },
+}
+</script>
